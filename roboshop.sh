385f10545e9d8525789ef8bdc76d5c9b95724caf
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-09-25 18:58:31 +0530

    roboshop-project

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,7 @@ do
     Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
-        Ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 
         'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 
