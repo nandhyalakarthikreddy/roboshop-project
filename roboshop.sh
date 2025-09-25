72d0e94beb3c3953c0c163bc3f4627fcc70fb37b
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-09-25 18:55:32 +0530

    roboshop-project

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ SG_ID="sg-03711d89ea0c3a77f"
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,
-    Tags=[{Key=Name,Value=$Instance}]" --query 'Instances[0].InstanceId' --output text)
+    Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
         Ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 
