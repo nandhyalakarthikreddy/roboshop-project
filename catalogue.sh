74e9c2490a158d39a7585b0befb9e3dbf1397c9e
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-10-14 15:35:06 +0530

    first commit

--- catalogue.sh
+++ catalogue.sh
@@ -36,7 +36,7 @@ VALIDATE $? "Enable nodejs"
 
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing nodejs"
-id roboshop
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "Addding system user"
