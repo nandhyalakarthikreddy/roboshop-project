eb3b3d52a0b8b73c22fdab4447be205d9264867b
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-10-14 15:08:47 +0530

    first commit

---
+++ catalogue.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+USERID=$(id -u)
+
+LOG_FOLDER="/var/log/shell-roboshop"
+FILE_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$FILE_NAME.log"
+MONGODB_HOST=mongodb.nkrdev.space
+mkdir -p $LOG_FOLDER
+
+echo "script started and executed at :  $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $R Error :: please run the script by using root user $N "
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R Error :: Failed to $2 server $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$G $2 server $N" | tee -a $LOG_FILE
+fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disable nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enable nodejs"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+VALIDATE $? "Addding system user"
+
+mkdir /app 
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? "downloading catalogue application"
+
+cd /app 
+VALIDATE $? "changing to app directory"
+
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzip the file"
+
+cd /app 
+VALIDATE $? "changing the app directory"
+
+npm install  &>>$LOG_FILE
+VALIDATE $? "installing the library"
+
+cp catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
+VALIDATE $? "Adding catalogue repo"
+
+systemctl daemon-reload
+VALIDATE $? "Reload the file"
+
+systemctl enable catalogue &>>$LOG_FILE
+VALIDATE $? "enable the catalogue"
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo 
+VALIDATE $? "Adding mongo repo"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "installing the mongodb"
+
+mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+VALIDATE $? "load catalogue products"
+
+systemctl restart catalogue
+VALIDATE $? "Restarting catalogue"
