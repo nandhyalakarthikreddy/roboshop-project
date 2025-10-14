2c6a4060033fdf5ce2e1521384b884e6ff6d9254
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-10-14 15:32:40 +0530

    first commit

--- catalogue.sh
+++ catalogue.sh
@@ -36,11 +36,13 @@ VALIDATE $? "Enable nodejs"
 
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing nodejs"
-
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-VALIDATE $? "Addding system user"
-
-mkdir /app 
+id roboshop
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Addding system user"
+else
+    echo -e "Already exists $Y skipping $N"
+mkdir -p /app 
 VALIDATE $? "creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
@@ -67,7 +69,7 @@ VALIDATE $? "Reload the file"
 systemctl enable catalogue &>>$LOG_FILE
 VALIDATE $? "enable the catalogue"
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo 
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
 VALIDATE $? "Adding mongo repo"
 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
