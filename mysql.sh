6af6f38087bab1a7b5d44f852997b02550a0852b
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-10-17 19:56:59 +0530

    first commit

---
+++ mysql.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+USERID=$(id -u)
+
+LOG_FOLDER="/var/log/shell-roboshop"
+FILE_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$FILE_NAME.log"
+START_TIME=$(date +%s)
+mkdir -p $LOG_FOLDER
+
+echo "script started and executed at :  $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $R Error :: please run the script by using root user $N "
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R Error :: Failed to $2 server $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$G $2 server $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install mysql-server -y
+VALIDATE $? "Installing my sql server"
+
+systemctl enable mysqld
+VALIDATE $? "Enabling mysql"
+
+systemctl start mysqld  
+VALIDATE $? "starting my sql server"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "setting up root password"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo -e "script executed in :: $Y $TOTAL_TIME seconds $N"
